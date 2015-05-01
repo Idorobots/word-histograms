f965ae876fce139e12a9723d1c204f14b96df29b
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2015-05-02 01:48:12 +0200

    Generate generators generating gratuitous generators.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ pip install -r requirements.txt
 
 Usage:
 ```
-python corpora.py --preprocess=all
-python histogram.py --list --syllables --output-suffix _histogram.json lang1.txt lang2.txt
+python corpora.py --preprocess bible
+python histogram.py --output-dir histograms/bible corpora/bible/*
 python similarity.py --input input.txt en.json pl.json es.json
 ```
--- histogram.py
+++ histogram.py
@@ -1,11 +1,21 @@
+from corpora import get_lang, get_langcode
 import getopt
 from hyphen import Hyphenator
 from hyphen import dictools
 import json
 import logging
+import os
 import os.path
 import sys
+import types
 
+def all_histograms(word_gens):
+    for obj in word_gens:
+        result = histograms(obj.words)
+        yield pack(obj.lang, *result)
+
+def pack(*args):
+    return args
 
 def histograms(word_gen):
     words = {}
@@ -37,33 +47,39 @@ def normalize(d):
 
     return {k: d[k]/frequency_sum for k in d}
 
-def by_word(input_file):
-    for word in open(input_file):
-        yield word.strip().lower()
+def by_word(input_gen):
+    for word in input_gen:
+        w = word.strip().lower()
+        logging.debug("word: {}".format(w))
+        yield w
 
-def by_syllable(input_file, install_lang_p):
-    lang = os.path.basename(input_file)
-
-    if install_lang_p and not dictools.is_installed(language):
+def by_syllable(input_gen, lang, install_lang_p):
+    if install_lang_p and not dictools.is_installed(lang):
         dictools.install(lang)
 
     hyphenator = Hyphenator(lang)
 
-    for word in open(input_file):
-        word = word.strip().lower()
+    for word in input_gen:
         syllables = hyphenator.syllables(word)
-        logging.debug("{} syllables: {}".format(word, syllables))
+        logging.debug("syllables: {}".format(syllables))
         for syllable in syllables:
             yield syllable
 
+def read_files(files, word_gen):
+    for f in files:
+        obj = types.SimpleNamespace()
+        obj.lang = get_lang(os.path.basename(f))
+        obj.words = word_gen(open(f))
+        yield obj
+
 if __name__ == "__main__":
-    options, args = getopt.getopt(sys.argv[1:], "", ["output-suffix=",
+    options, args = getopt.getopt(sys.argv[1:], "", ["output-dir=",
                                                      "install", "syllables",
                                                      "list",
                                                      "debug"])
     options = dict(options)
 
-    words_gen = by_word
+    word_gen = by_word
 
     logging_format = "%(levelname)s: %(message)s"
     if "--debug" in options:
@@ -72,19 +88,22 @@ if __name__ == "__main__":
         logging.basicConfig(format=logging_format, level=logging.INFO)
 
     if "--syllables" in options:
-        words_gen = lambda input_file: by_syllable(input_file, "--install" in options)
+        word_gen = lambda input_file: by_syllable(by_word(input_file),
+                                                  os.path.basename(input_file), # FIXME Hax :(
+                                                  "--install" in options)
 
-    for input_file in args:
-        all_words, all_lengths, unique_lengths = histograms(words_gen(input_file))
+    if "--output-dir" in options and not os.path.isdir(options["--output-dir"]):
+        os.makedirs(options["--output-dir"])
 
+    for lang, all_words, all_lengths, unique_lengths in all_histograms(read_files(args, word_gen)):
         result = {"lengths" : all_lengths,
                   "unique_lengths" : unique_lengths}
 
         if "--list" in options:
             result["words"] = all_words
 
-        if "--output-suffix" in options:
-            output_file = input_file + options["--output-suffix"]
+        if "--output-dir" in options:
+            output_file = os.path.join(options["--output-dir"], get_langcode(lang) + ".json")
             logging.info("Saving file {}.".format(output_file))
 
             with open(output_file, "w") as out:
