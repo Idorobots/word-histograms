fb037b2b5d0fdfa7b3db15e5e017389157e411c1
Michal Rus <m@michalrus.com>
2015-05-06 01:36:00 +0200

    Plot similarity charts for JSONs outputted from similarity.py
    Closes #4

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ Usage:
 ```
 python corpora.py --preprocess bible
 python histogram.py --output-dir histograms/bible corpora/bible/*
-python similarity.py --input unknown.txt histograms/bible/*
+python similarity.py --input unknown.txt --output unknown.json histograms/bible/*
+python charts.py --input unknown.json
 ```
---
+++ charts.py
@@ -0,0 +1,35 @@
+import getopt
+import json
+import sys
+import os.path
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    options, args = getopt.getopt(sys.argv[1:], '', ['input='])
+    options = dict(options)
+
+    input_file = sys.stdin
+    title = 'STDIN'
+    if '--input' in options:
+        input_file = open(options['--input'])
+        title = options['--input']
+
+    scores = json.load(input_file)
+
+    langs = sorted(scores, key = lambda k: scores[k]['score'])
+    langs_abbr = [os.path.splitext(os.path.basename(lang))[0] for lang in langs]
+    ys = [scores[lang]['score'] for lang in langs]
+    xs = np.arange(len(langs))
+
+    fig, ax = plt.subplots()
+
+    bar1 = ax.bar(xs, ys, 0.3, alpha = 0.4, color = 'b', label = 'Similarity')
+    ax.set_xlabel('Language')
+    ax.set_ylabel('Score')
+    ax.set_title(title)
+    ax.set_xticks(xs + 0.3)
+    ax.set_xticklabels(langs_abbr, rotation = 90)
+
+    plt.tight_layout()
+    plt.show()
